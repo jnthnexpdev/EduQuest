b10175cda2d686615116065380d2e003eb0426fc
Castro Arellano Angel Emanuel <angelemanuel444@gmail.com>
2024-05-10 14:03:53 -0600

    Added Footer and minor changes to home

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { SystemService } from '../../../shared/services/system/system.service';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  darkTheme = signal(false);
+
+  constructor(
+    private systemService : SystemService
+  ){}
+
+  ngOnInit() : void {
+    this.systemService.preferences$.subscribe((preferences : any) => {
+      this.getPreferences();
+    });
+  }
+
+  getPreferences(){
+    this.darkTheme.set(this.systemService.getThemeState());
+  }
 
 }
