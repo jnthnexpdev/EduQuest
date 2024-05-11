68ca760275759d2154e6d1defc407a0a95847060
Jesus Noriega <jesusnoriega0101@gmail.com>
2024-05-10 21:06:09 -0600

    footer changes

--- src/app/shared/components/scroll-top/scroll-top.component.ts
+++ src/app/shared/components/scroll-top/scroll-top.component.ts
@@ -1,19 +1,37 @@
-import { Component, HostListener  } from '@angular/core';
+import { Component, HostListener,OnInit,signal  } from '@angular/core';
+import { SystemService } from '../../../shared/services/system/system.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-scroll-top',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './scroll-top.component.html',
   styleUrl: './scroll-top.component.css'
 })
-export class ScrollTopComponent {
+export class ScrollTopComponent implements OnInit {
+  darkTheme = signal(false);
+
+  constructor(
+    private systemService : SystemService
+  ){}
+
+  ngOnInit() : void {
+    this.systemService.preferences$.subscribe((preferences : any) => {
+      this.getPreferences();
+    });
+  }
+
+  getPreferences(){
+    this.darkTheme.set(this.systemService.getThemeState());
+  }
+
   showScrollButton: boolean = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const yOffset = window.pageYOffset;
-    const scrollTrigger = window.innerHeight * 0.2; // Muestra el botón cuando el usuario ha hecho scroll más allá del 20% del alto de la ventana
+    const scrollTrigger = window.innerHeight * 0.2;
     this.showScrollButton = yOffset > scrollTrigger;
   }
 
