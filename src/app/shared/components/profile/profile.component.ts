4c660f2b1313a7695268cddfbe951741241465d5
Jesus Noriega <jesusnoriega0101@gmail.com>
2024-05-14 19:56:17 -0600

    design profile

--- src/app/shared/components/profile/profile.component.ts
+++ src/app/shared/components/profile/profile.component.ts
@@ -4,30 +4,51 @@ import { NgClass } from '@angular/common';
 import { MenuDashboardComponent } from '../../../shared/components/menu-dashboard/menu-dashboard.component';
 import { ProfileCoursesComponent } from '../../../courses/components/profile-courses/profile-courses.component';
 import { ProfileForumComponent } from '../../../forum/components/profile-forum/profile-forum.component';
+import { PlansComponent } from '../../../shared/components/plans/plans.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NgClass, MenuDashboardComponent,ProfileCoursesComponent,ProfileForumComponent],
+  imports: [NgClass, MenuDashboardComponent, ProfileCoursesComponent, ProfileForumComponent, PlansComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
 
   darkTheme = signal(false);
+  profileImage: string = '/assets/img/profile/user.png';
+  originalProfileImage: string = '/assets/img/profile/user.png';
+  showSaveButton: boolean = false;
+
 
   constructor(
-    private systemService : SystemService
-  ){}
+    private systemService: SystemService
+  ) { }
 
-  ngOnInit() : void {
-    this.systemService.preferences$.subscribe((preferences : any) => {
+  ngOnInit(): void {
+    this.systemService.preferences$.subscribe((preferences: any) => {
       this.getPreferences();
     });
   }
 
-  getPreferences(){
+  getPreferences() {
     this.darkTheme.set(this.systemService.getThemeState());
   }
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.profileImage = reader.result as string;
+        this.showSaveButton = true;
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  cancel(): void {
+    this.profileImage = this.originalProfileImage; // Restaura la imagen original
+    this.showSaveButton = false;
+  }
 }
