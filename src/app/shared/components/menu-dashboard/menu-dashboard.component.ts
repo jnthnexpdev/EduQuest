ea5d049892a4c15fa1593d86ec4954f56cd564e2
Jesus Noriega <jesusnoriega0101@gmail.com>
2024-05-16 13:00:05 -0600

    estructura  y colores de foro

--- src/app/shared/components/menu-dashboard/menu-dashboard.component.ts
+++ src/app/shared/components/menu-dashboard/menu-dashboard.component.ts
@@ -1,85 +1,86 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+  import { Component, OnInit, signal } from '@angular/core';
+  import { Router, NavigationEnd } from '@angular/router';
 
-import { NgClass } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { AlertService } from '../../../shared/services/alert/alert.service';
-import { SystemService } from '../../../shared/services/system/system.service';
-import { AuthServiceService } from '../../../auth/services/auth/auth-service.service';
-import { NotificationsComponent } from '../../../forum/components/notifications/notifications.component';
-import { ProfileForumComponent } from '../../../forum/components/profile-forum/profile-forum.component';
-import { ProfileCoursesComponent } from '../../../courses/components/profile-courses/profile-courses.component';
+  import { NgClass } from '@angular/common';
+  import { MatDialog } from '@angular/material/dialog';
+  import { AlertService } from '../../../shared/services/alert/alert.service';
+  import { SystemService } from '../../../shared/services/system/system.service';
+  import { AuthServiceService } from '../../../auth/services/auth/auth-service.service';
+  import { NotificationsComponent } from '../../../forum/components/notifications/notifications.component';
+  import { ProfileForumComponent } from '../../../forum/components/profile-forum/profile-forum.component';
+  import { ProfileCoursesComponent } from '../../../courses/components/profile-courses/profile-courses.component';
 
-@Component({
-  selector: 'app-menu-dashboard',
-  standalone: true,
-  imports: [NgClass, NotificationsComponent, ProfileForumComponent, ProfileCoursesComponent],
-  templateUrl: './menu-dashboard.component.html',
-  styleUrl: './menu-dashboard.component.css'
-})
-export class MenuDashboardComponent implements OnInit {
+  @Component({
+    selector: 'app-menu-dashboard',
+    standalone: true,
+    imports: [NgClass, NotificationsComponent, ProfileForumComponent, ProfileCoursesComponent],
+    templateUrl: './menu-dashboard.component.html',
+    styleUrl: './menu-dashboard.component.css'
+  })
+  export class MenuDashboardComponent implements OnInit {
 
-  darkTheme = signal(false);
-  showIcon: boolean = false;
-  currentContext: string = 'cursos';
-  
+    darkTheme = signal(false);
+    showIcon: boolean = false;
+    currentContext: string = 'cursos';
+    
 
-  constructor(
-    private systemService: SystemService,
-    private alertService: AlertService,
-    private authService: AuthServiceService,
-    private router: Router,
-    private matDialog: MatDialog,
-  ) { }
+    constructor(
+      private systemService: SystemService,
+      private alertService: AlertService,
+      private authService: AuthServiceService,
+      private router: Router,
+      private matDialog: MatDialog,
+    ) { }
 
-  ngOnInit(): void {
-    this.systemService.preferences$.subscribe((preferences: any) => {
-      this.getPreferences();
-    });
+    ngOnInit(): void {
+      this.systemService.preferences$.subscribe((preferences: any) => {
+        this.getPreferences();
+      });
 
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.updateIconState();
-        this.updateContext(); 
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.updateIconState();
+          this.updateContext(); 
+        }
+      });
+      this.updateIconState();
+      this.updateContext();
+    }
+
+
+    getPreferences() {
+      this.darkTheme.set(this.systemService.getThemeState());
+    }
+
+    updateIconState() {
+      const currentUrl = this.router.url;
+      this.showIcon = currentUrl.startsWith('/foro') && currentUrl !== '/cursos';
+    }
+
+    updateContext() {
+      const currentUrl = this.router.url;
+      if (currentUrl.includes('foro')) {
+        this.currentContext = 'foro';
+      } else if (currentUrl.includes('cursos')) {
+        this.currentContext = 'cursos';
       }
-    });
-    this.updateIconState();
-    this.updateContext();
-  }
+    }
 
-  getPreferences() {
-    this.darkTheme.set(this.systemService.getThemeState());
-  }
+    redirectToHome() {
+      this.authService.userLogOut();
+      this.router.navigate(['/inicio']);
+    }
 
-  updateIconState() {
-    const currentUrl = this.router.url;
-    this.showIcon = currentUrl.startsWith('/foro') && currentUrl !== '/cursos';
-  }
+    redirectToForum() {
+      this.router.navigate(['/foro/inicio']);
+    }
+    
+    redirectToCourses() {
+      this.router.navigate(['/cursos/inicio']);
+    }
 
-  updateContext() {
-    const currentUrl = this.router.url;
-    if (currentUrl.includes('foro')) {
-      this.currentContext = 'foro';
-    } else if (currentUrl.includes('cursos')) {
-      this.currentContext = 'cursos';
+    navigateToProfile(from: string): void {
+      this.currentContext = from;
+      this.router.navigate(['/eduquest/perfil'], { queryParams: { from: from } });
     }
   }
-
-  redirectToHome() {
-    this.authService.userLogOut();
-    this.router.navigate(['/inicio']);
-  }
-
-  redirectToForum() {
-    this.router.navigate(['/foro/inicio']);
-  }
-  
-  redirectToCourses() {
-    this.router.navigate(['/cursos/inicio']);
-  }
-
-  navigateToProfile(from: string): void {
-    this.currentContext = from;
-    this.router.navigate(['/eduquest/perfil'], { queryParams: { from: from } });
-  }
-}
